1741325005d5d4e3ac0e80ca3e6fea74a0f71391
swaraj-2895 <65616505+swaraj-2895@users.noreply.github.com>
2020-07-05 11:37:09 +0530

    Add files via upload

--- utills.py
+++ utills.py
@@ -1,8 +1,9 @@
-def headings(link):
-  htmlfile_source=requests.get(link).text
-  soup=BeautifulSoup(htmlfile_source,'lxml')
-  article_head=soup.find('h1', class_='display-heading-04').text.strip()
-  return (article_head)
+import transformers
+from bs4 import BeautifulSoup
+import transformers
+from transformers import pipeline
+import time
+import requests
 
 def get_links():
     links=[]
@@ -14,35 +15,36 @@ def get_links():
        links.append(source_link+article.a['href'])
     return links
 
-def pred():
-    links=get_links()
+def pred(links):
     text_sum=[]
     head_sum=[]
     for link in links:
-                 try:  
-                    htmlfile=requests.get(link).text
-                    soup=BeautifulSoup(htmlfile,'lxml')
-                    article_body=soup.find('div', class_='article-large-10 columns article-wrapper')
-                    article_head=soup.find('h1', class_='display-heading-04')
-                    articles=article_body.find_all('p')
-                    sentences=[]
-                    articles_to_sum=""
-                    final_article=[]
-                    for art in articles[:10]:
-                        articles_to_sum=articles_to_sum+" "+art.text
-                    summarizer=pipeline("summarization")
-                    text=summarizer(articles_to_sum, max_len=120, min_len=30)
-                    for i in text:
-                        for key, value in i.items():
-                            text_sum.append(value)
-
-                    head=headings(link)
-                    head_sum.append(head)
-              except:  
-                    print("Connection refused by the server..")
-                    print("Let me sleep for 5 seconds")
-                    print("ZZzzzz...")
-                    time.sleep(5)
-                    print("Was a nice sleep, now let me continue...")
-                    continue
-              return(head_sum, text_sum)
+        try:  
+            htmlfile=requests.get(link).text
+            soup=BeautifulSoup(htmlfile,'lxml')
+            article_body=soup.find('div', class_='article-large-10 columns article-wrapper')
+            article_head=soup.find('h1', class_='display-heading-04')
+            articles=article_body.find_all('p')
+            sentences=[]
+            articles_to_sum=""
+            final_article=[]
+            for art in articles[:10]:
+                articles_to_sum=articles_to_sum+" "+art.text
+                summarizer=pipeline("summarization")
+                text=summarizer(articles_to_sum, max_len=120, min_len=30)
+                for i in text:        
+                    for key, value in i.items():
+                        text_sum.append(value)
+            htmlfile_source=requests.get(link).text
+            soup=BeautifulSoup(htmlfile_source,'lxml')
+            head=soup.find('h1', class_='display-heading-04').text.strip()
+            head_sum.append(head)
+            
+        except:  
+            print("Connection refused by the server..")
+            print("Let me sleep for 5 seconds")
+            print("ZZzzzz...")
+            time.sleep(5)
+            print("Was a nice sleep, now let me continue...")
+            continue
+    return(head_sum, text_sum)

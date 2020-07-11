d28f3299f2aefb80629b8efc1863179f77f5bf0a
swaraj-2895 <65616505+swaraj-2895@users.noreply.github.com>
2020-07-11 12:08:19 +0530

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ from flask import jsonify
 from flask import Flask, render_template, url_for
 from app import r
 from app import q
+from app import tasks
 from tasks import get_links
 from tasks import pred
 

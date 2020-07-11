1867dbba52e26484ce4e71f28dcaa0cec02d1a58
swaraj-2895 <65616505+swaraj-2895@users.noreply.github.com>
2020-07-11 15:00:41 +0530

    Add files via upload

--- run.py
+++ run.py
@@ -1,4 +1,31 @@
 from app import app
+import flask
+from flask import request
+from flask import jsonify
+from flask import Flask, render_template, url_for
+from app import r
+from app import q
+from tasks import get_links
+from tasks import pred
+import redis
+from rq import Queue
+
+apps=Flask(__name__)
+r = redis.Redis()
+
+q = Queue(connection=r)
+
+@apps.route('/')
+def index():
+    return render_template('text_sum.html')
+
+@apps.route('/pred', methods=['POST'])
+def pred():
+    if request.method=='POST':
+        links = q.enqueue(get_links)
+        head_sum, text_sum=q.enqueue(pred, links) 
+    return render_template('text_sum.html', head_sum=head_sum, text_sum=text_sum)
+
 
 if __name__ == "__main__":
     app.run()
